277592868dc1c15201b6bd661e2bbd3ed252f07d
zkoo <149389112+hqwi@users.noreply.github.com>
2024-04-02 15:04:09 +0200

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -14,7 +14,7 @@ class help(commands.Cog):
     @bot.slash_command(name="help", description="📚 Display the list of all of our commands.")
     async def help(self, ctx):
         embed = nextcord.Embed(title="📚 Command List:", timestamp=datetime.utcnow(), color=nextcord.Colour.green())
-        embed.set_footer(text="Free Version: https://github.com/zkoolol/Account-Generator-Bot", icon_url="https://cdn.discordapp.com/attachments/1204244617869000788/1204249308237402162/icon.png?ex=65d40b90&is=65c19690&hm=42115bccb811f86d7a9cdbcb686a2f1c2a5be6633e04f712da3765d6da475e03&")
+        embed.set_footer(text="Free Version: https://github.com/hqwi/Account-Generator-Bot", icon_url="https://cdn.discordapp.com/attachments/1204244617869000788/1204249308237402162/icon.png?ex=65d40b90&is=65c19690&hm=42115bccb811f86d7a9cdbcb686a2f1c2a5be6633e04f712da3765d6da475e03&")
         embed.add_field(name="/fgen", value="Generates accounts from the free generating service.", inline=False)
         embed.add_field(name="/fstock", value="Displays the list of our account stock.", inline=False)
         embed.add_field(name="/help", value="Displays this command.", inline=False)
